9db676ba3c4cac7333ed9a27ff1d370080def324
kenchaso <kenchasonakai@gmail.com>
2023-07-09 12:41:30 +0900

    ギア比計算の追加

---
+++ chapter2/gear_ratio.rb
@@ -0,0 +1,9 @@
+chainring = 52                    # 歯数
+cog       = 11
+ratio     = chainring / cog.to_f
+puts ratio                        # -> 4.72727272727273
+
+chainring = 30
+cog       = 27
+ratio     = chainring / cog.to_f
+puts ratio                        # -> 1.11111111111111
