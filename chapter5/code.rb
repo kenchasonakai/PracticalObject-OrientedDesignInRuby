52140c3ecb9116c4fd55f92244fd4c978df7eb12
kenchaso <kenchasonakai@gmail.com>
2023-07-21 21:58:21 +0900

    add

--- chapter5/code.rb
+++ chapter5/code.rb
@@ -119,3 +119,23 @@ class Trip
     }
   end
 end
+
+# kind_of?とis_a?
+if preparer.kind_of?(Mechanic)
+  preparer.prepare_bicycles(bicycle)
+elsif preparer.kind_of?(TripCoordinator)
+  preparer.buy_food(customers)
+elsif preparer.kind_of?(Driver)
+  preparer.gas_up(vehicle)
+  preparer.fill_water_tank(vehicle)
+end
+
+# respond_to?
+if preparer.responds_to?(:prepare_bicycles)
+  preparer.prepare_bicycles(bicycle)
+elsif preparer.responds_to?(:buy_food)
+  preparer.buy_food(customers)
+elsif preparer.responds_to?(:gas_up)
+  preparer.gas_up(vehicle)
+  preparer.fill_water_tank(vehicle)
+end
