8eba7e16310572696c311ed4a25b9413e2f8f9f8
kenchaso <kenchasonakai@gmail.com>
2023-07-21 21:33:27 +0900

    add: chapter5

---
+++ chapter5/code.rb
@@ -0,0 +1,23 @@
+# ダックを見逃す
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
+
+  # この 'mechanic' 引数はどんなクラスのものでもよい。
+  def prepare(mechanic)
+    mechanic.prepare_bicycles(bicycles)
+  end
+
+  # ...
+end
+
+# *この* クラスのインスタンスを渡すことになったとしても、
+# 動作する。
+class Mechanic
+  def prepare_bicycles(bicycles)
+    bicycles.each {|bicycle| prepare_bicycle(bicycle)}
+  end
+
+  def prepare_bicycle(bicycle)
+    #...
+  end
+end
