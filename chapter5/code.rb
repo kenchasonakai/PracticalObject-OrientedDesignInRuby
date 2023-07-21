8be442c291665b786459bddbfc746bc4cbe26287
kenchaso <kenchasonakai@gmail.com>
2023-07-21 21:36:23 +0900

    add

--- chapter5/code.rb
+++ chapter5/code.rb
@@ -1,4 +1,5 @@
 # ダックを見逃す
+
 class Trip
   attr_reader :bicycles, :customers, :vehicle
 
@@ -18,6 +19,44 @@ class Mechanic
   end
 
   def prepare_bicycle(bicycle)
+    #...
+  end
+end
+
+# 問題を悪化させる
+
+# 旅行の準備はさらに複雑になった
+class Trip
+  attr_reader :bicycles, :customers, :vehicle
+
+  def prepare(preparers)
+    preparers.each {|preparer|
+      case preparer
+      when Mechanic
+        preparer.prepare_bicycles(bicycles)
+      when TripCoordinator
+        preparer.buy_food(customers)
+      when Driver
+        preparer.gas_up(vehicle)
+        preparer.fill_water_tank(vehicle)
+      end
+    }
+  end
+end
+
+# TripCoordinator と Driver を追加した
+class TripCoordinator
+  def buy_food(customers)
+    # ...
+  end
+end
+
+class Driver
+  def gas_up(vehicle)
+    #...
+  end
+
+  def fill_water_tank(vehicle)
     #...
   end
 end
