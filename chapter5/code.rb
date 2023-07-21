9d9123a0d5125357c6649d34c798fa5d5c3fc087
kenchaso <kenchasonakai@gmail.com>
2023-07-21 22:02:22 +0900

    add

--- chapter5/code.rb
+++ chapter5/code.rb
@@ -139,3 +139,20 @@ elsif preparer.responds_to?(:gas_up)
   preparer.gas_up(vehicle)
   preparer.fill_water_tank(vehicle)
 end
+
+# 賢くダックを選ぶ
+# <tt>find(:first, *args)</tt> を便利に使うためのラッパー。
+# このメソッドには <tt>find(:first)</tt> メソッドと
+# 同じ引数をすべて渡せる
+def first(*args)
+  if args.any?
+    if args.first.kind_of?(Integer) ||
+         (loaded?&& !args.first.kind_of?(Hash))
+      to_a.first(*args)
+    else
+      apply_finder_options(args.first).first
+    end
+  else
+    find_first
+  end
+end
