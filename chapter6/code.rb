3c91b3eda39406071b9e0a81bb20479388c4f74a
kenchaso <kenchasonakai@gmail.com>
2023-07-30 14:06:40 +0900

    add

--- chapter6/code.rb
+++ chapter6/code.rb
@@ -132,6 +132,18 @@ end
 class MountainBike < Bicycle
   # Bicycle のサブクラスのまま（Bicycle は現在空になっている）。
   # コードは何も変更されていない。
+
+  # attr_reader :front_shock, :rear_shock
+
+  # def initialize(args)
+  #   @front_shock = args[:front_shock]
+  #   @rear_shock  = args[:rear_shock]
+  #   super(args)
+  # end
+
+  # def spares
+  #   super.merge(rear_shock: rear_shock)
+  # end
 end
 
 road_bike = RoadBike.new(
