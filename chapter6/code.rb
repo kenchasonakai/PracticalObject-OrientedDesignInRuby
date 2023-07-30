69c272df880afad1cffedb3105729843f7076b33
kenchaso <kenchasonakai@gmail.com>
2023-07-30 14:34:20 +0900

    add

--- chapter6/code.rb
+++ chapter6/code.rb
@@ -300,3 +300,104 @@ bent = RecumbentBike.new
 #  NotImplementedError:
 #    This RecumbentBike cannot respond to:
 #             'default_tire_size'
+
+# 結合度を理解する
+class RoadBike < Bicycle
+  # ...
+  def spares
+    { chain:        '10-speed',
+      tire_size:    '23',
+      tape_color:   tape_color}
+  end
+end
+
+class MountainBike < Bicycle
+  # ...
+  def spares
+    super.merge({rear_shock:  rear_shock})
+  end
+end
+
+class Bicycle
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(args={})
+    @size       = args[:size]
+    @chain      = args[:chain]     || default_chain
+    @tire_size  = args[:tire_size] || default_tire_size
+  end
+
+  def spares
+    { tire_size:  tire_size,
+      chain:      chain}
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def initialize(args)
+    @tape_color = args[:tape_color]
+    super(args)
+  end
+
+  def spares
+    super.merge({ tape_color: tape_color})
+  end
+
+  def default_tire_size
+    '23'
+  end
+end
+
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock =  args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge({rear_shock: rear_shock})
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+end
+
+class RecumbentBike < Bicycle
+  attr_reader :flag
+
+  def initialize(args)
+    @flag = args[:flag]  #  'super' を送信するのを忘れた
+  end
+
+  def spares
+    super.merge({flag: flag})
+  end
+
+  def default_chain
+    '9-speed'
+  end
+
+  def default_tire_size
+    '28'
+  end
+end
+
+bent = RecumbentBike.new(flag: 'tall and orange')
+bent.spares
+# -> {:tire_size => nil, <- 初期化されていない
+#     :chain     => nil,
+#     :flag      => "tall and orange"}
