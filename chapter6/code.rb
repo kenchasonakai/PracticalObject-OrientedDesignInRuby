5b0dff221179e76071e0ea5319c82b0923e74c27
kenchaso <kenchasonakai@gmail.com>
2023-07-30 13:29:46 +0900

    add

--- chapter6/code.rb
+++ chapter6/code.rb
@@ -1,3 +1,4 @@
+# 具象クラスから始める
 class Bicycle
   attr_reader :size, :tape_color
 
@@ -26,3 +27,41 @@ bike.spares
 # -> {:tire_size   => "23",
 #     :chain       => "10-speed",
 #     :tape_color  => "red"}
+
+# 複数の型を埋め込む
+class Bicycle
+  attr_reader :style, :size, :tape_color,
+              :front_shock, :rear_shock
+
+  def initialize(args)
+    @style        = args[:style]
+    @size        = args[:size]
+    @tape_color  = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock  = args[:rear_shock]
+  end
+
+  # “style”の確認は、危険な道へ進む一歩
+  def spares
+    if style == :road
+      { chain:        '10-speed',
+        tire_size:    '23',       # milimeters
+        tape_color:   tape_color }
+    else
+      { chain:        '10-speed',
+        tire_size:    '2.1',      # inches
+        rear_shock:   rear_shock }
+    end
+  end
+end
+
+bike = Bicycle.new(
+        style:        :mountain,
+        size:         'S',
+        front_shock:  'Manitou',
+        rear_shock:   'Fox')
+
+bike.spares
+# -> {:tire_size   => "2.1",
+#     :chain       => "10-speed",
+#     :rear_shock  => 'Fox'}
